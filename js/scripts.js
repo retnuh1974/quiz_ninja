bb6f7b9d8f4c90e86216ddec637f8bd5d691fd7d
retnuh1974 <retnuh1974@comcast.net>
2015-09-07 08:08:23 -0400

    End of Chapter 4 updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,43 @@
 var quiz = [
-    ["What is Superman's real name?","Clarke Kent"],
-    ["What is Wonderwoman's real name?","Dianna Prince"],
-    ["What is Batman's real name?","Bruce Wayne"]
-    ];
-   
+  ["What is Superman's real name?","Clarke Kent"],
+  ["What is Wonderwoman's real name?","Dianna Prince"],
+  ["What is Batman's real name?","Bruce Wayne"]
+  ];
+  
 var score = 0 // initialize score
 
-for(var i=0,max=quiz.length;i<max;i++){
-  
-  // get answer from user
-  var answer = prompt(quiz[i][0]); // quiz[i][0] is the ith questions
+play(quiz);
 
-  // check if answer is correct
-  if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
-    alert("Correct!");
-    // increase score by 1
-    score++;
-  } else {
-    alert("Wrong!");
+//// function definitions ////
+
+function play(quiz){
+  // main game loop
+  for(var i=0, question, answer, max=quiz.length; i<max; i++) {
+    question = quiz[i][0];
+    answer = ask(question);
+    check(answer);
+  }
+  // end of main game loop
+  gameOver();
+  
+  // nested functions
+  
+  function ask(question) {
+    return prompt(question); // quiz[i][0] is the ith questions
+  }
+
+  function check(answer) {
+    if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
+      alert("Correct!");
+      // increase score by 1
+      score++;
+    } else {
+      alert("Wrong!");
+    }
+  }
+
+  function gameOver(){
+    // inform the player that the game has finished and tell them how many points they have scored
+    alert("Game Over, you scored " + score + " points");
   }
 }
-
-// Tell user the score
-alert("Game Over, you scored " + score + " points");
